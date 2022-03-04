935d77091278e9cccc94b9f2966dc75ff92e1995
patrickYoo <yhy1045@naver.com>
2022-03-04 22:57:14 +0900

    [add] 어뎁터 패턴 업데이트

--- src/test/java/me/patrick/laboratory/javapattern/adapterpattern/SampleAdapterPattern.java
+++ src/test/java/me/patrick/laboratory/javapattern/adapterpattern/SampleAdapterPattern.java
@@ -1,7 +1,56 @@
 package me.patrick.laboratory.javapattern.adapterpattern;
 
+import org.junit.jupiter.api.Test;
+
 public class SampleAdapterPattern {
 
+    public interface Auton{
+        void detailing();
+        void visit();
+    }
 
-    
+    public static class AutonImpl implements Auton{
+
+        @Override
+        public void detailing() {
+            System.out.println("내부 세차로직");
+        }
+
+        @Override
+        public void visit() {
+            System.out.println("기사 방문");
+        }
+    }
+
+    public interface InstaWash{
+        void carWash();
+        void delivery();
+    }
+
+    public static class InstaWashImpl implements InstaWash{
+
+        private final Auton auton;
+
+        public InstaWashImpl(Auton auton) {
+            this.auton = auton;
+        }
+
+        @Override
+        public void carWash() {
+            System.out.println("차량 외부 세차로직");
+            auton.detailing();
+        }
+
+        @Override
+        public void delivery() {
+            System.out.println("차량 배달 로직");
+        }
+    }
+
+    @Test
+    void executor(){
+        InstaWash instaWash = new InstaWashImpl(new AutonImpl());
+        instaWash.carWash();
+        instaWash.delivery();
+    }
 }
