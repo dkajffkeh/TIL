40d7d8bae83dbb6a13782e0fff69399ac5c5186a
patrickYoo <yhy1045@naver.com>
2023-03-02 23:40:47 +0900

    [add] testCommit

--- src/main/java/me/patrick/laboratory/mvctest/controller/MemberController.java
+++ src/main/java/me/patrick/laboratory/mvctest/controller/MemberController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @AllArgsConstructor
 @Slf4j
