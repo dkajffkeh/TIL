7919b2b9d61463075d335b69db245f7a0208a68b
patrickYoo <yhy1045@naver.com>
2024-04-03 00:39:52 +0900

    feat: TIL 프로젝트 정상화

--- src/main/java/me/patrick/laboratory/LaboratoryApplication.java
+++ src/main/java/me/patrick/laboratory/LaboratoryApplication.java
@@ -10,25 +10,6 @@ public class LaboratoryApplication {
         SpringApplication.run(LaboratoryApplication.class, args);
     }
 
-/*    @Bean
-    public ApplicationRunner applicationRunner() {
-        return args -> {
-            System.out.println("Api요청");
-
-            Map<String,String> map = new HashMap<>();
-
-            Mono<String> stringMono = webClient.get()
-                    .uri(URI.create("https://localhost:8889/kotlin/test"))
-                    .accept(MediaType.APPLICATION_JSON)
-                    .retrieve()
-                    .bodyToMono(String.class);
-
-            Disposable disposable = stringMono.doOnSuccess(System.out::println).subscribe();
-
-            System.out.println("haha");
-        };
-    }*/
-
 }
 
 
